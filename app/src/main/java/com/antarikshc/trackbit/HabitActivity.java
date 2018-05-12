d9a530ee0f9c4eeb0b4c4ced4581205ee0d3901e
Antariksh C <antarikshc@gmail.com>
2018-05-13 02:08:02 +0530

    Added Dialog to quickly update habit -Single click: Dialog -Long Click: EditorActivity
    Signed-off-by: Antariksh C <antarikshc@gmail.com>

--- app/src/main/java/com/antarikshc/trackbit/HabitActivity.java
+++ app/src/main/java/com/antarikshc/trackbit/HabitActivity.java
@@ -1,5 +1,6 @@
 package com.antarikshc.trackbit;
 
+import android.app.AlertDialog;
 import android.app.LoaderManager;
 import android.content.ContentValues;
 import android.content.Intent;
@@ -8,12 +9,16 @@ import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.Window;
 import android.view.animation.AccelerateInterpolator;
 import android.view.animation.LinearInterpolator;
 import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
 
@@ -72,9 +77,109 @@ public class HabitActivity extends AppCompatActivity implements LoaderManager.Lo
         loaderManager = getLoaderManager();
         loaderManager.initLoader(HABITS_LOADER_ID, null, this);
 
+        //get current day
+        Calendar calendar = Calendar.getInstance();
+        final int currentDay = calendar.get(Calendar.DAY_OF_WEEK);
+
         habitsList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+                LayoutInflater factory = LayoutInflater.from(HabitActivity.this);
+                final View dialogView = factory.inflate(R.layout.popup_dialog, null);
+                final AlertDialog dialog = new AlertDialog.Builder(HabitActivity.this).create();
+
+                dialog.setView(dialogView);
+
+                //we don't want title
+                dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+
+                dialog.setContentView(R.layout.popup_dialog);
+
+                //bind views from dialog layout
+                final TextView dialogHabitNumber = dialogView.findViewById(R.id.dialog_habit_number);
+                ImageView dialogAdd = dialogView.findViewById(R.id.dialog_add_button);
+                ImageView dialogSub = dialogView.findViewById(R.id.dialog_sub_button);
+                Button dialogSave = dialogView.findViewById(R.id.dialog_save);
+
+                //get habit records and update habit number textview
+                HabitData currentHabit = customAdapter.getItem(position);
+                final Integer records[] = currentHabit.getHabitDays();
+                dialogHabitNumber.setText(String.valueOf(records[currentDay - 1]));
+
+                final Integer habitId = currentHabit.getHabitId();
+
+                dialogAdd.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        int i = Integer.parseInt(dialogHabitNumber.getText().toString());
+                        i += 1;
+                        dialogHabitNumber.setText(String.valueOf(i));
+                    }
+                });
+
+                dialogSub.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        int i = Integer.parseInt(dialogHabitNumber.getText().toString());
+                        i -= 1;
+                        dialogHabitNumber.setText(String.valueOf(i));
+                    }
+                });
+
+                dialogSave.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+
+                        //just close dialog box if user hasn't changed value
+                        if (dialogHabitNumber.getText().toString().equals
+                                (String.valueOf(records[currentDay - 1])
+                                )) {
+                            dialog.dismiss();
+                        } else {
+
+                            //get value from textview and update records array
+                            records[currentDay - 1] = Integer.parseInt(dialogHabitNumber.getText().toString());
+
+                            //using column array instead of switch-case to update habit of current day
+                            String[] columnNames = {HabitEntry.COLUMN_SUNDAY,
+                                    HabitEntry.COLUMN_MONDAY,
+                                    HabitEntry.COLUMN_TUESDAY,
+                                    HabitEntry.COLUMN_WEDNESDAY,
+                                    HabitEntry.COLUMN_THURSDAY,
+                                    HabitEntry.COLUMN_FRIDAY,
+                                    HabitEntry.COLUMN_SATURDAY};
+
+                            SQLiteDatabase db = mDbHelper.getWritableDatabase();
+
+                            ContentValues values = new ContentValues();
+
+                            //good thing that we have column names array xD
+                            for (int i = 0; i <= 6; i++) {
+                                values.put(columnNames[i], records[i]);
+                            }
+
+                            db.update(HabitEntry.TABLE_NAME, values,
+                                    HabitEntry._ID + " = ?",
+                                    new String[]{String.valueOf(habitId)});
+
+                            db.close();
+
+                            loaderManager.restartLoader(HABITS_LOADER_ID, null, HabitActivity.this);
+
+                            dialog.dismiss();
+                        }
+                    }
+                });
+
+                dialog.show();
+
+            }
+        });
+
+        habitsList.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
 
                 HabitData currentHabit = customAdapter.getItem(position);
 
@@ -90,14 +195,12 @@ public class HabitActivity extends AppCompatActivity implements LoaderManager.Lo
 
                     Integer records[] = currentHabit.getHabitDays();
 
-                    //get current day
-                    Calendar calendar = Calendar.getInstance();
-                    int currentDay = calendar.get(Calendar.DAY_OF_WEEK);
                     intent.putExtra("dayNum", records[currentDay - 1]);
 
                     startActivity(intent);
 
                 }
+                return true;
             }
         });
 
@@ -109,6 +212,7 @@ public class HabitActivity extends AppCompatActivity implements LoaderManager.Lo
         });
 
         mDbHelper = new HabitDbHelper(this);
+
     }
 
     @Override
